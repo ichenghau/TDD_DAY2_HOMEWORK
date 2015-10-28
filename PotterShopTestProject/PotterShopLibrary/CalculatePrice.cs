1164cfe26d9e3408be5ecfcb66a7484bd319e839
ChengHau <creator45648@gmail.com>
2015-10-28 23:43:18 +0800

    完成計算銷售組合邏輯

--- PotterShopTestProject/PotterShopLibrary/CalculatePrice.cs
+++ PotterShopTestProject/PotterShopLibrary/CalculatePrice.cs
@@ -7,9 +7,82 @@ namespace PotterShopLibrary
 {
     public class Calculate: ICalculatePrice
     {
-        public double Sum(List<Book> shop)
+        public double Sum(List<Book> shops)
         {
-            throw new NotImplementedException();
+            double sum = 0;
+
+            while (shops.Any())
+            {
+                sum += 計算一個集合的總和(ref shops);
+            }
+
+            return sum;
+        }
+
+        private double 計算一個集合的總和(ref List<Book> shops)
+        {
+            int count = 0;
+
+            var 第一集 = shops.FirstOrDefault(w => w.Type == type.第一集);
+            if (第一集 != null)
+            {
+                count++;
+                shops.Remove(第一集);
+            }
+
+            var 第二集 = shops.FirstOrDefault(w => w.Type == type.第二集);
+            if (第二集 != null)
+            {
+                count++;
+                shops.Remove(第二集);
+            }
+
+            var 第三集 = shops.FirstOrDefault(w => w.Type == type.第三集);
+            if (第三集 != null)
+            {
+                count++;
+                shops.Remove(第三集);
+            }
+
+            var 第四集 = shops.FirstOrDefault(w => w.Type == type.第四集);
+            if (第四集 != null)
+            {
+                count++;
+                shops.Remove(第四集);
+            }
+
+            var 第五集 = shops.FirstOrDefault(w => w.Type == type.第五集);
+            if (第五集 != null)
+            {
+                count++;
+                shops.Remove(第五集);
+            }
+
+            return 計算群組總和搭配總和(count);
+        }
+
+        private double 計算群組總和搭配總和(int count)
+        {
+            switch (count)
+            {
+                case 1:
+                    return count * 100 * 1;
+
+                case 2:
+                    return count * 100 * 0.90;
+
+                case 3:
+                    return count * 100 * 0.85;
+
+                case 4:
+                    return count * 100 * 0.80;
+
+                case 5:
+                    return count * 100 * 0.75;
+
+                default:
+                    return count * 100;
+            }
         }
     }
 }
